03b62b5b35f6890cd3d983ace0b3e995a747d40b
Odai Mohammed Alkhalaf Alissa <odaia@hiof.no>
2024-10-30 09:23:05 +0100

    update

--- frontend/src/ config/urls.tsx
+++ frontend/src/ config/urls.tsx
@@ -1,3 +1,2 @@
 export const API_BASE_URL = 'http://localhost:3000';
 export const PROJECTS_URL = `${API_BASE_URL}/projects`;
-
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import ProjectListPage from './pages/ProjectListPage';
 import CreateProjectPage from './pages/CreateProjectPage';
+import AboutMe from './components/AbutMe';
 import './Style/Style.css';
 
 export default function App() {
@@ -9,7 +10,15 @@ export default function App() {
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={<h2>Velkommen til Prosjektportefølje</h2>} />
+          <Route 
+            path="/" 
+            element={
+              <>
+                <h2>Velkommen til Prosjektportefølje</h2>
+                <AboutMe /> {/* Legg til AboutMe på hovedsiden */}
+              </>
+            } 
+          />
           <Route path="/projects" element={<ProjectListPage />} />
           <Route path="/create-project" element={<CreateProjectPage />} />
         </Routes>
